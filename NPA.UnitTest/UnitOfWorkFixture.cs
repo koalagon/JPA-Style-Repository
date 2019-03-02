bd466efa6766aab68e4003ce9820512734941728
Brian Kwak <koalagon@gmail.com>
2019-03-01 17:48:49 -0700

    Scan assembly when start-up to improve the performance

---
+++ NPA.UnitTest/UnitOfWorkFixture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NPA.Repository;
+using NUnit.Framework;
+
+namespace NPA.UnitTest
+{
+    [TestFixture]
+    public class UnitOfWorkFixture
+    {
+        [Test]
+        public void Constructor_ShouldThrowException_WhenNameOrConnectionStringIsNull()
+        {
+            Assert.Throws<InvalidOperationException>(() => new UnitOfWork());
+        }
+    }
+}
